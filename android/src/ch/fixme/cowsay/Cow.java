3939e93423cf687c35a506483bec20c4d652b90f
Rorist <aubort.jeanbaptiste@gmail.com>
2011-12-27 10:19:30 +0100

    change all methods to private but get_cow, add comment for future dev

--- android/src/ch/fixme/cowsay/Cow.java
+++ android/src/ch/fixme/cowsay/Cow.java
@@ -6,32 +6,56 @@ public class Cow
     String cow = "default"; //.cow
     String eyes = "oo";
     String tongue = "  ";
-
-    String message = "I Love Android";
+    String[] message;
     
     int face;
-    private final int FACE_BORG = 1;
-    private final int FACE_DEAD = 2;
-    private final int FACE_GREEDY = 3;
-    private final int FACE_PARANOID = 4;
-    private final int FACE_STONED = 5;
-    private final int FACE_TIRED = 6;
-    private final int FACE_WIRED = 7;
-    private final int FACE_YOUNG = 8;
+    final int FACE_BORG = 1;
+    final int FACE_DEAD = 2;
+    final int FACE_GREEDY = 3;
+    final int FACE_PARANOID = 4;
+    final int FACE_STONED = 5;
+    final int FACE_TIRED = 6;
+    final int FACE_WIRED = 7;
+    final int FACE_YOUNG = 8;
 
-    void list_cowfiles() {
+    // up-left, up-right, down-left, down-right, left, right
+    final char[] border = new char[] { '/', '\\', '\\', '/', '|', '|' };
+    final String format = "%s %s-%s %s\n";
+
+    public Cow(String message) {
+        // Process message 
+        this.message = new String[]{};
+        // Construct balloon
+        // Construct face
     }
 
-    void slurp_input() {
+    public Cow(String message, String cow, String eyes, String tongue) {
+        this.cow = cow;
+        this.eyes = eyes;
+        this.tongue = tongue;
+        super(message);
+    }
+    
+    public String get_cow() { // The method used in Main.java to get the constructed cow
+        return "";
     }
 
-    void maxlen() {
+
+    private void list_cowfiles() {
     }
 
-    void construct_balloon() {
+    private void slurp_input() {
     }
 
-    void construct_face() {
+    private int maxlen() {
+        return 10;
+    }
+
+    private void construct_balloon() {
+        int maxlen = maxlen();
+    }
+
+    private void construct_face() {
         switch(face){
             case FACE_BORG:
                 eyes = "==";
@@ -54,10 +78,6 @@ public class Cow
         }
     }
 
-    String get_cow() {
-        return "";
-    }
-
-    void display_usage() {
+    private void display_usage() {
     }
 }
